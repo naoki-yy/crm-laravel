1e3858583c18937b678d07b9825f57b33ec77d11
yamada <naoki19980422@gmail.com>
2024-06-22 15:40:08 +0900

    Inertia-vuejs

---
+++ resources/js/Pages/Inertia/Show.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { router } from '@inertiajs/vue3'
+defineProps({
+    id : String,
+    blog : Object,
+})
+
+const deleteConfirm = (id) => {
+    router.delete(`/inertia/${id}`,{
+        onBefore: () => confirm('本当に削除しますか？')
+    })
+}
+</script>
+
+<template>
+{{ id }}
+{{ blog.title }}
+<button @click="deleteConfirm(blog.id)" class="bg-blue-200">削除</button>
+</template>
